8e0364149e5982118f6a3125f87439f68607046d
aleiadevore <2496@holbertonschool.com>
2021-09-06 16:42:06 -0500

    Camera works

--- 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -23,20 +23,22 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Steady cam follows player
         transform.position = Vector3.Lerp(transform.position, target.position+target_Offset, 0.1f);
 
+        // Rotates when mouse is dragged
         if (Input.GetMouseButtonDown(0))
         {
             transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * dragSpeed, -Input.GetAxis("Mouse X") * dragSpeed, 0));
             return;
         }
-        //if (!Input.GetMouseButton(0)) return;
+        if (!Input.GetMouseButton(0)) return;
         X = transform.rotation.eulerAngles.x;
         Y = transform.rotation.eulerAngles.y;
         transform.rotation = Quaternion.Euler(X, Y, 0);
 
-        /*Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
         Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
-        transform.Rotate(move, Space.World);*/
+        transform.Rotate(-move, Space.World);
     }
 }
