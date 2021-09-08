1d7bc190c4b387cc481a5773d97d862b38397a14
aleiadevore <2496@holbertonschool.com>
2021-09-08 12:27:43 -0500

    Updating respawn method

--- 0x05-3rd_person_copy/Assets/Scripts/PlayerController.cs
+++ 0x05-3rd_person_copy/Assets/Scripts/PlayerController.cs
@@ -4,54 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject player;
+    /*public GameObject player;
     public Transform spawnPoint;
+    public GameObject cap;*/
 
 
     // Start is called before the first frame update
     void Start()
     {
-        /*rb = GetComponent<Rigidbody>();
-        isGrounded = true;*/
-    }
-
-    /// <summary>
-    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
-    /// </summary>
-    void FixedUpdate()
-    {
-        //Debug.Log(isGrounded);
-        /*if (isGrounded == true)
-        {
-            rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Jump") * jumpHeight, Input.GetAxis("Vertical") * speed);
-        }*/
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if (player.transform.position.y < -6)
+    {     
+        //cap.GetComponent<MeshRenderer>().material.color = Color.red;
+        if (transform.position.y < -6)
         {
-            player.transform.position = spawnPoint.position;
-            /*rb.velocity = new Vector3(0, 0, 0);*/
+            transform.position = new Vector3(0, 10, 0);
         }
     }
 
-    void OnCollisionEnter(Collision other)
-    {
-        /*Debug.Log("Enter");
-        //Debug.Log("Grounded");
-        if (other.gameObject.tag=="Floor")
-            isGrounded = true;
-        Debug.Log(isGrounded);*/
-    }
-
-    void OnCollisionExit(Collision other)
-    {
-        /*Debug.Log("Exit");
-        //Debug.Log("Not Grounded");
-        if (other.gameObject.tag=="Floor")
-            isGrounded = false;
-        Debug.Log(isGrounded);*/
-    }
 }
