7ecb3bfbfcee4736ab4579ea544a61ccd2f1cf95
aleiadevore <2496@holbertonschool.com>
2021-09-21 14:56:25 -0500

    Level buttons now work

---
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LevelOne()
+    {
+        Debug.Log("Clicked");
+        SceneManager.LoadScene("Level01");
+    }
+
+    void LevelTwo()
+    {
+        SceneManager.LoadScene("Level02");
+    }
+
+    void LevelThree()
+    {
+        SceneManager.LoadScene("Level03");
+    }
+}
