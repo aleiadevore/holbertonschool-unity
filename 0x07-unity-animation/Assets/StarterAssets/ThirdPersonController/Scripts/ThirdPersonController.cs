60042b68b2dec705ded5259e1e69bc7ed96d0469
aleiadevore <2496@holbertonschool.com>
2021-10-06 11:33:28 -0500

    Idle to running anim works

--- 0x07-unity-animation/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ 0x07-unity-animation/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -222,7 +222,7 @@ namespace StarterAssets
 			// update animator if using character
 			if (_hasAnimator)
 			{
-				_animator.SetFloat(_animIDSpeed, _animationBlend);
+				_animator.SetFloat(_animIDSpeed, _speed);
 				_animator.SetFloat(_animIDMotionSpeed, inputMagnitude);
 			}
 		}
