36141737c27c9dae22f71fdf7db9559acda09d81
aleiadevore <2496@holbertonschool.com>
2021-10-21 18:04:38 -0500

    MainMenu audio prefs now works

--- 0x08-unity-audio/Assets/Scripts/MainMenu.cs
+++ 0x08-unity-audio/Assets/Scripts/MainMenu.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer Mixer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        /* Reset sound to player prefs */
+        float BGMVol = PlayerPrefs.GetFloat("BGMVol");
+        float SFXVol = PlayerPrefs.GetFloat("SFXVol");
+        Mixer.SetFloat("BGMVol", Mathf.Log10(BGMVol) * 20);
+        Mixer.SetFloat("SFXVol", Mathf.Log10(SFXVol) * 20);
     }
 
     // Update is called once per frame
