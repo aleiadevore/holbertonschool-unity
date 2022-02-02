e6a3f9bd1992d100bcd1822defb62650f0be7474
aleiadevore <2496@holbertonschool.com>
2022-02-02 17:10:20 -0600

    Cleaning code

--- 0x09-unity_ar_business_card/Assets/Scripts/ButtonScript.cs
+++ 0x09-unity_ar_business_card/Assets/Scripts/ButtonScript.cs
@@ -6,17 +6,7 @@ public class ButtonScript : MonoBehaviour
 {
     public string url;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    /// <summary>Opens url</summary>
     public void VisitSite()
     {
         Application.OpenURL(url);
