eb5bbea0fb7e1ee3d64af6c6d469ae477da077c2
aleiadevore <2496@holbertonschool.com>
2022-02-02 11:54:23 -0600

    Adding documentation and comments

--- 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
+++ 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
@@ -11,26 +11,33 @@ public class CutsceneController : MonoBehaviour
     public GameObject player;
     private GameObject cutsceneCamera;
     public GameObject mainCamera;
-    // Start is called before the first frame update
+
+    /// <summary>Sets variable values at start of scene</summary>
     void Start()
     {
+        // Finds CutsceneCamera
         cutsceneCamera = GameObject.Find("CutsceneCamera");
+        // Finds scene number
         Scene scene = SceneManager.GetActiveScene();
         int sceneNumber = scene.buildIndex;
+        // Subtracts 1 to get level number and gives it to animator
         animator.SetInteger("Scene", sceneNumber - 1);
         Debug.Log(animator.GetInteger("Scene"));
     }
 
-    // Update is called once per frame
+    /// <summary>Handles completion of animation</summary>
     void Update()
     {
         if(animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
         { 
-            //If normalizedTime is 0 to 1 means animation is playing, if greater than 1 means finished
+            // If normalizedTime is 0 to 1 means animation is playing, if greater than 1 means finished
             Debug.Log("Animation complete");
+            // Prepares timer
             timerCanvas.gameObject.SetActive(true);
+            // Enables player movement
             player.GetComponent<PlayerController>().enabled = true;
             player.GetComponent<ThirdPersonController>().enabled = true;
+            // Switches from animation camera to game camera
             mainCamera.gameObject.SetActive(true);
             cutsceneCamera.gameObject.SetActive(false);
         }
